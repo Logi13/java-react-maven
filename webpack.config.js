b9b5c79b6704f3c9f44970b661f6f536be38b6c1
john <johnny.lioup@gmail.com>
2024-03-11 12:18:24 +0000

    Fixed issue with webpack not loading css

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var path = require('path');
 
 module.exports = {
-    entry: ['./frontend/src/main/js/app.js', './frontend/src/main/js/client.js', './frontend/src/main/js/follow.js'],
-    devtool: 'sourcemaps',
-    cache: true,
+    entry: ['./frontend/src/main/js/app.js', './frontend/src/main/js/client.js', './frontend/src/main/js/follow.js', './frontend/src/main/js/resources/static/main.css'],
+    // devtool: 'sourcemaps',
+    // cache: true,
     mode: 'development',
     output: {
-        path: __dirname,
-        filename: './backend/src/main/resources/static/built/bundle.js'
+        path: path.resolve(__dirname, './backend/src/main/resources/static/built'),
+        filename: 'bundle.js'
+        // path: __dirname,
+        // filename: './backend/src/main/resources/static/built/bundle.js'
     },
     module: {
         rules: [
             {
-                test: path.join(__dirname, '.'),
+                // test: path.join(__dirname, '.'),
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, './frontend/src/main/js'),
                 exclude: /(node_modules)/,
                 use: [{
                     loader: 'babel-loader',
@@ -20,6 +24,11 @@ module.exports = {
                         presets: ["@babel/preset-env", "@babel/preset-react"]
                     }
                 }]
+            },
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, './frontend/src/main/js/resources/static'), // specify the path to your CSS file
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
